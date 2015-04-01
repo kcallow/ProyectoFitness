c86312899aad238fd47bb511de377585d2e38a78
Kenneth Callow <kcallow@ic-itcr.ac.cr>
2015-04-01 17:46:54 -0600

    No se

--- src/proyectofitness/Dia.java
+++ src/proyectofitness/Dia.java
@@ -23,6 +23,30 @@ public class Dia {
         throw new Exception("Indice invalido.  No se ha modificado la lista de ejercicios...");
     }
 
+    public void modificarEjercicio(Ejercicio ejercicio) throws Exception {
+        try {
+            borrarEjercicio(ejercicio.getNumero());
+            agregarEjercicio(ejercicio);
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    public String verEjercicio(int numero) throws Exception {
+        if(numero == 0){
+            String resultado = "";
+            for(Ejercicio ejercicio : ejercicios)
+                resultado += ejercicio.toString() + "\n";
+            return resultado;
+        }
+        else {
+            for(Ejercicio ejercicio : ejercicios)
+                if(ejercicio.getNumero() == numero)
+                    return ejercicio.toString();
+            throw new Exception("Numero no corresponde a ningun ejercicio registrado.");
+        }
+    }
+
     @Override
     public String toString(){
         return ejercicios.toString();

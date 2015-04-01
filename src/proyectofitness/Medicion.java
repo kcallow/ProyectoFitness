8388693257c16e3982b8e0c0e1153c1eefa225aa
scsaenz <scsaenz94@gmail.com>
2015-04-01 08:41:17 -0600

    c:

--- src/proyectofitness/Medicion.java
+++ src/proyectofitness/Medicion.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectofitness;
 
 /**
@@ -10,7 +5,40 @@ package proyectofitness;
  * @author callow
  */
 public class Medicion {
+<<<<<<< HEAD
     
     
     
+=======
+
+    private final String nombre, unidadMedida;
+    private int valor;
+
+    public Medicion(String nombre, String unidadMedida, int valor) {
+        this.nombre = nombre;
+        this.unidadMedida = unidadMedida;
+        this.valor = valor;
+    }
+
+    public Medicion(String nombre, String unidadMedida) {
+        //Si no se da el valor, se inicializa a 0
+        this(nombre, unidadMedida, 0);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getUnidadMedida() {
+        return unidadMedida;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+>>>>>>> 4636cd7e1fc05af967c9e6504d4b80aa5011404c
 }
